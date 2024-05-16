c702479a1329b2f342689322a6d1ea9a3d3ac7e0
sergiofrubio <sergiofrubio@gmail.com>
2024-05-16 10:38:50 +0200

    corrigo errores menores

--- app/src/main/java/com/sfr/practicas_singlab_android_kotlin_jetpack_compose/presentation/login/state/LoginState.kt
+++ app/src/main/java/com/sfr/practicas_singlab_android_kotlin_jetpack_compose/presentation/login/state/LoginState.kt
@@ -1,15 +1,7 @@
 package com.sfr.practicas_singlab_android_kotlin_jetpack_compose.presentation.login.state
 
-sealed class Login{
-    object Idle: Login()
-    object Loading: Login()
-    data class Success(val message: String): Login()
-    data class Error (val message: String): Login()
-
-    data class LoginState(
+data class LoginState(
         val email: String = "",
         val password: String = "",
         val isLoginButtonEnabled: Boolean = false
-    )
-
-}
+)
--- app/src/main/java/com/sfr/practicas_singlab_android_kotlin_jetpack_compose/presentation/login/viewModel/LoginViewModel.kt
+++ app/src/main/java/com/sfr/practicas_singlab_android_kotlin_jetpack_compose/presentation/login/viewModel/LoginViewModel.kt
@@ -1,26 +1,20 @@
 package com.sfr.practicas_singlab_android_kotlin_jetpack_compose.presentation.login.viewModel
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.sfr.practicas_singlab_android_kotlin_jetpack_compose.domain.useCases.usesLogin
 import com.sfr.practicas_singlab_android_kotlin_jetpack_compose.presentation.login.event.LoginEvent
-import com.sfr.practicas_singlab_android_kotlin_jetpack_compose.presentation.login.state.Login
 import com.sfr.practicas_singlab_android_kotlin_jetpack_compose.presentation.login.state.LoginState
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(private var usesLogin: usesLogin) : ViewModel() {
 
-    private var _loginState = MutableStateFlow<Login>(Login.Idle)
-    val loginState: StateFlow<Login> = _loginState.asStateFlow()
+    private var _loginState = MutableStateFlow(LoginState())
+    val loginState = _loginState.asStateFlow()
 
      fun onEvent(event: LoginEvent) {
         when(event) {
